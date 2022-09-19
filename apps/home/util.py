899fb9fd54957c103e2a808624ba8fe27228ad80
kchongee <kchongee@gmail.com>
2022-09-19 20:37:45 +0800

    fix getting session key

--- apps/home/util.py
+++ apps/home/util.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, flash, url_for, session, redirect, url_for
 
 def output_flash_msg():
-    if session["flash_msg"]:
-        flash(session["flash_msg"]["msg"], session["flash_msg"]["type"])
+    if session.get("flash_msg"):
+        flash(session.get("flash_msg")["msg"], session.get("flash_msg")["type"])
         session.pop("flash_msg")
