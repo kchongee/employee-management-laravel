1e3502bf0a16128a6e482346b5e7009174cf6a3f
kchongee <kchongee@gmail.com>
2022-09-24 01:34:45 +0800

    change to production

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -48,37 +48,37 @@ class Users(db.Model, UserMixin):
     def to_json(self):        
         return json.dumps(self.as_dict())
 
-# class Employees(db.Model):
+class Employees(db.Model):
 
-#     __tablename__ = 'employees'
+    __tablename__ = 'employees'
 
-#     id = db.Column(db.Integer, primary_key=True)    
-#     full_name = db.Column(db.String(64), unique=True)        
-#     contact = db.Column(db.Integer)
-#     address = db.Column(db.String(64), unique=True)
-#     # city = db.Column(db.String(64), unique=True)
-#     # country = db.Column(db.String(64), unique=True)
-#     # postal_code = db.Column(db.String(64), unique=True)
-#     department = db.Column(db.String(64))
-#     job = db.Column(db.String(64))
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    id = db.Column(db.Integer, primary_key=True)    
+    full_name = db.Column(db.String(64), unique=True)        
+    contact = db.Column(db.Integer)
+    address = db.Column(db.String(64), unique=True)
+    # city = db.Column(db.String(64), unique=True)
+    # country = db.Column(db.String(64), unique=True)
+    # postal_code = db.Column(db.String(64), unique=True)
+    department = db.Column(db.String(64))
+    job = db.Column(db.String(64))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-#     def __init__(self, **kwargs):
-#         for property, value in kwargs.items():
-#             # depending on whether value is an iterable or not, we must
-#             # unpack it's value (when **kwargs is request.form, some values
-#             # will be a 1-element list)
-#             if hasattr(value, '__iter__') and not isinstance(value, str):
-#                 # the ,= unpack of a singleton fails PEP8 (travis flake8 test)
-#                 value = value[0]             
+    def __init__(self, **kwargs):
+        for property, value in kwargs.items():
+            # depending on whether value is an iterable or not, we must
+            # unpack it's value (when **kwargs is request.form, some values
+            # will be a 1-element list)
+            if hasattr(value, '__iter__') and not isinstance(value, str):
+                # the ,= unpack of a singleton fails PEP8 (travis flake8 test)
+                value = value[0]             
 
-#             setattr(self, property, value)
+            setattr(self, property, value)
 
-#     def __repr__(self):
-#         return str(self.full_name)
+    def __repr__(self):
+        return str(self.full_name)
 
-#     def as_dict(self):
-#        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+    def as_dict(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 class Departments(db.Model):
 
